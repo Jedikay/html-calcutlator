bbd8f0e0cdac30fbbe31b4134c75e2926149881f
Jedikay <kadyns0604@gmail.com>
2024-03-08 22:15:41 -0600

    feat: added javascript to make calc function correctly

--- calculator.js
+++ calculator.js
@@ -1,31 +1,48 @@
 let runningTotal = 0;
 let buffer = "0";
-let previousOperator;
+let previousOperator = null;
 
 const screen = document.querySelector(".screen");
 
 function buttonClick(value) {
   if (isNaN(value)) {
     // this is not a number
+    handleSymbol(value);
   } else {
     // this is a number
-
     handleNumber(value);
   }
   screen.innerText = buffer;
 }
 
 function handleSymbol(symbol) {
-    console.log('handleSymbol', symbol);
-  switch (value) {
+  console.log('handleSymbol', symbol);
+  switch (symbol) {
     case "C":
-      buffer = "0";
+      buffer = '0';
       runningTotal = 0;
       break;
+    case "=":
+      if (previousOperator === null) {
+       // need you two numbers to do math
+       return; 
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = runningTotal;
+      runningTotal = 0;
+      break;
+      case '←':
+        if (buffer.length === 1) {
+          buffer = '0';
+        } else {
+          buffer = buffer.substring(0, buffer.length - 1);
+        }
+        break;
     case "+":
-    case "-":
-    case "&times":
-    case "&divide":
+    case "−":
+    case "×":
+    case "÷":
       handleMath(symbol);
       break;
   }
@@ -52,11 +69,12 @@ function handleMath(symbol) {
 }
 
 function flushOperation(intBuffer) {
+  console.log(intBuffer);
   if (previousOperator === "+") {
     runningTotal += intBuffer;
-  } else if (previousOperator === "-") {
+  } else if (previousOperator === "−") {
     runningTotal -= intBuffer;
-  } else if (previousOperator === "&times") {
+  } else if (previousOperator === "×") {
     runningTotal *= intBuffer;
   } else {
     runningTotal /= intBuffer;
